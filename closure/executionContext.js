baed67616e6afca72c192349e42e1390dbc856a3
Medha Adepu <medha.adepucse@gmail.com>
2025-11-23 17:07:05 +0530

    closure

---
+++ closure/executionContext.js
@@ -0,0 +1,14 @@
+let age = 45;
+
+function displayAge() {
+    console.log("Age is: " + age);
+}
+displayAge();
+
+function changeAge(){
+    age = 50;
+    console.log("Age changed to: " + age);
+}
+changeAge();
+
+
