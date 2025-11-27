e8c3e3a04b8f27f8e11451f9ba352a5219a1d6f4
Medha Adepu <medha.adepucse@gmail.com>
2025-11-27 22:19:17 +0530

    Remove node_modules from repo

---
+++ asyncI/q15.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync")();
+
+let sec = parseInt(prompt('Enter number of seconds: '));
+console.log(sec);
+
+let count = 0;
+let timer = setInterval(() => {
+    console.log("Time remaining: " + (sec - count));
+    count++;
+    if(count > sec) {
+        clearInterval(timer);
+        console.log("Countdown Complete !")
+        return;
+    }
+
+    setTimeout(() => {
+        let input = prompt("Press s to stop: ");
+        if(input === 's') {
+            clearInterval(timer);
+            consolele.log("Countdown stopped by user!");
+        }
+    }, 0);
+}, 1000);
